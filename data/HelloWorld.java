2610625e1a41c4a902aa95d556c995bb9cd320e7
Ryan Pinsker <cjumper555@gmail.com>
2022-08-26 08:30:40 -0700

    First commit with new project

---
+++ data/HelloWorld.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList<String> stringlist = new ArrayList<String>();
+		System.out.println("Hello World");
+	}
+
+}
