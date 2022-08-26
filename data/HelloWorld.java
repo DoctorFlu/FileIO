9e0d0541790632668751bbf6b5076b3ea231a273
Ryan Pinsker <cjumper555@gmail.com>
2022-08-26 08:45:26 -0700

    Fun stuff added

--- data/HelloWorld.java
+++ data/HelloWorld.java
@@ -5,7 +5,12 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		ArrayList<String> stringlist = new ArrayList<String>();
+		int var = 69;
 		System.out.println("Hello World");
+		System.out.println("What's up gang");
+		if (var < 420) {
+			System.out.println("lmao zedong");
+		}
 	}
 
 }
